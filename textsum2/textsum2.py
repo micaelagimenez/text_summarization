3aff1b57457ce26689b5035e922a6eb430f9290c
Micaela <micaelagimenez@live.com.ar>
2020-01-28 14:32:46 -0300

    initial commit

---
+++ textsum2/textsum2.py
@@ -0,0 +1,67 @@
+#Import packages
+import bs4 as bs
+import urllib.request
+import re
+import heapq
+import nltk.data
+
+#Install nltk specific modules
+nltk.download('punkt')
+nltk.download("stopwords")
+
+#Scrape the data
+scraped_data = urllib.request.urlopen('https://en.wikipedia.org/wiki/Histrionic_personality_disorder')
+article = scraped_data.read()
+
+#Parse the data
+parsed_article = bs.BeautifulSoup(article, 'lxml')
+paragraphs = parsed_article.find_all('p')
+article_text = ""
+
+for p in paragraphs:
+    article_text += p.text
+
+#Remove square brackets and extra spaces
+article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+article_text = re.sub(r'\s+', ' ', article_text)
+
+#Remove special characters and digits
+formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
+formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+
+#Sentence tokenization
+sentence_list = nltk.sent_tokenize(article_text)
+
+#Find frequency of occurrence
+stopwords = nltk.corpus.stopwords.words('english')
+
+word_frequencies = {}
+for word in nltk.word_tokenize(formatted_article_text):
+    if word not in stopwords:
+        if word not in word_frequencies.keys():
+            word_frequencies[word] = 1
+        else:
+            word_frequencies[word] += 1
+
+#Find weighted frequency
+maximum_frequency = max(word_frequencies.values())
+
+for word in word_frequencies.keys():
+    word_frequencies[word] = (word_frequencies[word]/maximum_frequency)
+
+#Calculate sentence scores
+sentence_scores = {}
+for sent in sentence_list:
+    for word in nltk.word_tokenize(sent.lower()):
+        if word in word_frequencies.keys():
+            if len(sent.split(' ')) < 30:
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word]
+                else:
+                    sentence_scores[sent] += word_frequencies[word]
+
+#Getting the summary
+summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+
+summary = ' '.join(summary_sentences)
+print(summary)
